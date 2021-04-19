1830a38995a386a57fca832ce796c0d660e43945
htlam <htlam164@gmail.com>
2021-04-19 21:42:18 +0700

    update

--- app/src/main/java/com/example/android_challenge_w2/Profile.kt
+++ app/src/main/java/com/example/android_challenge_w2/Profile.kt
@@ -1,6 +1,7 @@
 package com.example.android_challenge_w2
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,57 +10,95 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.*
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.customdialog.view.*
+import androidx.lifecycle.Observer
+
+import com.example.android_challenge_w2.databinding.ActivityProfileBinding
+import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.customdialog.view.*
 
-class Profile : AppCompatActivity() {
+class Profile : LogIn() {
+    private  lateinit var  binding: ActivityProfileBinding
+    private  lateinit var  viewModel: ProfileViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        supportActionBar?.hide();
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_profile)
+        viewModel=ViewModelProvider(this).get(ProfileViewModel::class.java)
 
+        val bundle=intent.extras
+        bundle?.let {
+            val emailOrgi=bundle.getString("email","unknow")
+            val fullNameOrgi=bundle.getString("fullName","unknow")
+            val passwordOrgi=bundle.getString("passWord","unknow")
 
-        val textView26=findViewById<TextView>(R.id.textView26)
-        val textView25=findViewById<TextView>(R.id.textView25)
-        val textView19=findViewById<TextView>(R.id.textView19)
-        val textView20=findViewById<TextView>(R.id.textView20)
-        val textView21=findViewById<TextView>(R.id.textView21)
-
-        textView26.setOnClickListener {
-            //Inflate the dialog with custom view
-            val mDialogView = LayoutInflater.from(this).inflate(R.layout.customdialog, null)
-            //AlertDialogBuilder
-            val mBuilder = AlertDialog.Builder(this)
-                .setView(mDialogView)
-                .setTitle("Edit Profile")
-            //show dialog
-            val  mAlertDialog = mBuilder.show()
-            //login button click of custom layout
-            mDialogView.dialogLoginBtn.setOnClickListener {
-                //dismiss dialog
-                mAlertDialog.dismiss()
-                //get text from EditTexts of custom layout
-                val name = mDialogView.dialogNameEt.text.toString()
-                val email = mDialogView.dialogEmailEt.text.toString()
-                val password = mDialogView.dialogPasswEt.text.toString()
-                //set the input text in TextView
-                //   mainInfoTv.setText("Name:"+ name +"\nEmail: "+ email +"\nPassword: "+ password)
-                textView25.setText(name)
-                textView19.setText(name)
-                textView20.setText(email)
-                textView21.setText(password)
+            if(viewModel.account.email.equals("default")){
+                viewModel.account.fullName=fullNameOrgi
+                viewModel.account.email=emailOrgi
+                viewModel.account.password=passwordOrgi
+                viewModel.account.numberPhone="enter your number phone"
 
             }
-            //cancel button click of custom layout
-            mDialogView.dialogCancelBtn.setOnClickListener {
-                //dismiss dialog
-                mAlertDialog.dismiss()
+
+
+
+            binding.account= viewModel.account
+
+
+
+
+            //button click to show dialog
+            textView26.setOnClickListener {
+                //Inflate the dialog with custom view
+                val mDialogView = LayoutInflater.from(this).inflate(R.layout.customdialog, null)
+                //AlertDialogBuilder
+                val mBuilder = AlertDialog.Builder(this)
+                    .setView(mDialogView)
+                    .setTitle("Edit Profile")
+                //show dialog
+                val  mAlertDialog = mBuilder.show()
+                //login button click of custom layout
+                mDialogView.dialogLoginBtn.setOnClickListener {
+                    //dismiss dialog
+                    mAlertDialog.dismiss()
+                    //get text from EditTexts of custom layout
+                    val name = mDialogView.dialogNameEt.text.toString()
+                    val email = mDialogView.dialogEmailEt.text.toString()
+                    val numberPhone = mDialogView.dialogPasswEt.text.toString()
+//------------------------------
+                    loginViewModel.getLoginDetails(context,emailOrgi,passwordOrgi)!!.observe(this, Observer {
+                        it.Email=email
+                        it.FullName=name
+
+
+                        viewModel.account.fullName=name
+                        viewModel.account.email=email
+                        viewModel.account.password=passwordOrgi
+                        viewModel.account.numberPhone=numberPhone
+                        binding.invalidateAll()
+                        binding.account=viewModel.account
+                    })
+
+
+                }
+                //cancel button click of custom layout
+                mDialogView.dialogCancelBtn.setOnClickListener {
+                    //dismiss dialog
+                    mAlertDialog.dismiss()
+                }
             }
         }
 
-        val imageView9=findViewById<ImageView>(R.id.imageView9)
+
+
         imageView9.setOnClickListener{
-            val intent = Intent(this, LogIn::class.java)
+
+            val intent=Intent(this,LogIn::class.java)
             startActivity(intent)
         }
+
+
     }
 }
